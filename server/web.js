0fd50c7a5770060c6ed4a627a045417ea2029bc3
Jan Miksovsky <jan@quickui.org>
2016-12-15 22:29:10 -0800

    Turn on simple client-side caching.

--- server/web.js
+++ server/web.js
@@ -21,8 +21,7 @@ app.get('/wiki/download/*', (request, response) => {
 
 /* Reference page */
 app.get('/reference/:title', (request, response) => {
-  wikiPage(request)
-  .then(result => response.send(result));
+  respondWithWikiPage(request, response);
 });
 
 /* Top-level of reference redirects to site home page. */
@@ -32,17 +31,45 @@ app.get('/reference', (request, response) => {
 
 /* Serve other top-level page */
 app.get('/:title', (request, response) => {
-  wikiPage(request)
-  .then(result => response.send(result));
+  respondWithWikiPage(request, response);
 });
 
 /* Serve up home page */
 app.get('/', (request, response) => {
   request.params.title = 'Home';
-  wikiPage(request)
-  .then(result => response.send(result));
+  respondWithWikiPage(request, response);
 });
 
 app.listen(port, () => {
   console.log(`Server listening on http://localhost:${port}`);
 });
+
+
+// Handle a web request by returning a wiki page.
+function respondWithWikiPage(request, response) {
+  // Render the request as content, or a promise for content.
+  let result = wikiPage(request);
+  // If the result's not already a promise, cast it to a promise.
+  Promise.resolve(result)
+  .then(content => {
+    // Return the content as the response.
+    response.set({
+      'Cache-Control': 'public,max-age=300', // Cache for 5 minutes
+      'Content-Type': inferContentType(content)
+    });
+    response.send(content);
+  });
+}
+
+// Given textual content to return, infer its Content-Type.
+function inferContentType(content) {
+  if (content.startsWith('<!DOCTYPE html>')) {
+    return 'text/html';
+  } else if (content.startsWith('<?xml')) {
+    return 'text/xml';
+  } else if (content.startsWith('{')) {
+    return 'application/json';
+  } else {
+    return 'text/plain';
+  }
+}
