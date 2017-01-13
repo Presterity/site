d3c4fb14564f725d722773585a9240bfd95d9f12
Jan Miksovsky <jan@quickui.org>
2017-01-12 20:08:13 -0800

    More refactoring

--- server/pages/topicLinks.js
+++ server/pages/topicLinks.js
@@ -1,5 +1,6 @@
 /*
- * Given a topic, renders the bookmarks tagged with that topic as HTML links.
+ * Given a topic, renders a promise to render the bookmarks tagged with that
+ * topic as HTML links.
  */
 
 
@@ -9,44 +10,42 @@ const bookmarks = require('../connectors/bookmarks');
 module.exports = (topic) => {
   return bookmarks.bookmarksForTopic(topic)
   .then(bookmarks => {
-
-    const links = bookmarks.map(bookmark => {
-
-      let domain = bookmark.domain;
-      if (domain.startsWith('www.')) {
-        domain = domain.slice(4); // Remove "www."
-      }
-      const { date, text } = parseLinkTitle(bookmark.title);
-
-      // Construct list of all tags except the one matching the topic.
-      const tags = bookmark.tags
-          .filter(tag => tag !== topic)
-          .map(tag => `<a href="/reference/${tag}">${tag}</a>`);
-      let tagsHtml = tags.join(', ');
-      if (tagsHtml.trim().length > 0) {
-        tagsHtml = `(See also ${tagsHtml})`;
-      }
-
-      return `<tr>
-        <td>${date}</td>
-        <td>
-          ${text}
-          <a href="${bookmark.link}">${domain}</a>
-          ${tagsHtml}
-        </td>
-      </tr>`;
-    });
-    
+    const links = bookmarks.map(bookmark => formatLinkForBookmark(bookmark, topic));
     const linksHtml = links.join('\n');
     return `<table class="topicLinks">
       ${linksHtml}
     </table>`;
-  })
-  .catch(exception => {
-    console.log(`Exception: ${exception}`);
   });
 };
 
+
+function formatLinkForBookmark(bookmark, topic) {
+
+  let domain = bookmark.domain;
+  if (domain.startsWith('www.')) {
+    domain = domain.slice(4); // Remove "www."
+  }
+  const { date, text } = parseLinkTitle(bookmark.title);
+
+  // Construct list of all tags except the one matching the topic.
+  const tags = bookmark.tags
+      .filter(tag => tag !== topic)
+      .map(tag => `<a href="/reference/${tag}">${tag}</a>`);
+  let tagsHtml = tags.join(', ');
+  if (tagsHtml.trim().length > 0) {
+    tagsHtml = `(See also ${tagsHtml})`;
+  }
+
+  return `<tr>
+    <td>${date}</td>
+    <td>
+      ${text}
+      <a href="${bookmark.link}">${domain}</a>
+      ${tagsHtml}
+    </td>
+  </tr>`;
+
+}
 
 function parseLinkTitle(title) {
   const linkPartsRegex = /([\d-]+) (.*)/;
