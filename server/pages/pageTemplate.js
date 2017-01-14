df97a8e9ee6741fb3e8ec1c91dc275cb896baf8f
Jan Miksovsky <jan@quickui.org>
2017-01-13 20:01:54 -0800

    Rename presterity.ico -> presterity.jpg to reflect its actual file type.

--- server/pages/pageTemplate.js
+++ server/pages/pageTemplate.js
@@ -27,7 +27,7 @@ module.exports = (request, data) => {
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width,initial-scale=1.0">
         <title>${title}</title>
-        <link rel="shortcut icon" href="/static/presterity.ico" type="image/x-icon" />
+        <link rel="shortcut icon" href="/static/presterity.jpg" type="image/x-icon" />
         <link rel="apple-touch-icon" sizes="144x144" href="/static/appIcon.png" />
         <link rel="stylesheet" href="/static/main.css">
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:400,700">
@@ -37,7 +37,7 @@ module.exports = (request, data) => {
         <header id="navigation">
           <div id="links">
             <a href="/">
-              <img id="logo" src="/static/presterity.ico">
+              <img id="logo" src="/static/presterity.jpg">
             </a>
             <div id="areaLinks">
               <a id="linkHome" href="/">Presterity</a>
