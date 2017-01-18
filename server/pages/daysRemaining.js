610363259239dd6fb8a35a116c7ac5cfed7f4bbd
Jan Miksovsky <jan@quickui.org>
2017-01-18 12:15:21 -0800

    Better date math.

--- server/pages/daysRemaining.js
+++ server/pages/daysRemaining.js
@@ -7,12 +7,17 @@ module.exports = () => {
 
   const today = new Date(Date.now());
   const inaugurationDate = new Date(Date.parse('Mon, Jan 20 2021 12:00:00 EST'));
-  const diffMilliseconds = inaugurationDate.getTime() - today.getTime();
+
+  // From "a more correct solution" for date diff math:
+  // http://stackoverflow.com/a/15289883/76472.
+  const utc1 = Date.UTC(today.getFullYear(), today.getMonth(), today.getDate());
+  const utc2 = Date.UTC(inaugurationDate.getFullYear(), inaugurationDate.getMonth(), inaugurationDate.getDate());
+  const diffMilliseconds = utc2 - utc1;
   const MILLISECONDS_PER_DAY = 24 * 60 * 60 * 1000;
   const diffDays = Math.floor(diffMilliseconds / MILLISECONDS_PER_DAY);
 
   if (diffDays === 0) {
-    return `This is the last day of the Trump administration!`;
+    return `<strong>This is the last day of the Trump administration!</strong>`;
   } else if (diffDays > 0) {
     const days = diffDays === 1 ? 'day' : 'days';
     return `There are only <strong>${diffDays}</strong> ${days} to go in the Trump administration.`;
