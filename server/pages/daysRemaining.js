569a495930f56a3609c8ad748456996b434886a3
Jan Miksovsky <jan@quickui.org>
2017-01-18 12:06:57 -0800

    Fix year of end of Trump administration.

--- server/pages/daysRemaining.js
+++ server/pages/daysRemaining.js
@@ -1,12 +1,12 @@
 /*
  * Return a string representing the number of days remaining until noon (EST) on
- * Inauguration Day, January 20, 2020.
+ * Inauguration Day, January 20, 2021.
  */
 
 module.exports = () => {
 
   const today = new Date(Date.now());
-  const inaugurationDate = new Date(Date.parse('Mon, Jan 20 2020 12:00:00 EST'));
+  const inaugurationDate = new Date(Date.parse('Mon, Jan 20 2021 12:00:00 EST'));
   const diffMilliseconds = inaugurationDate.getTime() - today.getTime();
   const MILLISECONDS_PER_DAY = 24 * 60 * 60 * 1000;
   const diffDays = Math.floor(diffMilliseconds / MILLISECONDS_PER_DAY);
